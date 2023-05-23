0a985b57d235bb775664e712486d1fa853f12afa
davemann100 <davemann100@gmail.com>
2023-05-22 20:49:25 -0700

    config

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,28 +1,33 @@
 // Registration.js
 import React, { useState } from "react";
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const Registration = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!firstName || !lastName || !email || !password) {
-      setError('Please fill in all fields.');
+      setError("Please fill in all fields.");
       return;
     }
 
     try {
-      const response = await axios.post('http://localhost:8000/api/register', { firstName, lastName, email, password });
+      const response = await axios.post("http://localhost:8000/api/register", {
+        firstName,
+        lastName,
+        email,
+        password,
+      });
       console.log(response.data); // Handle the response as needed
-      navigate('/'); // Redirect to the login page or any other page
+      navigate("/"); // Redirect to the login page or any other page
     } catch (error) {
       console.error(error);
     }
@@ -32,17 +37,37 @@ const Registration = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name:</label>
-        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
         <label htmlFor="lastName">Last Name:</label>
-        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
         <label htmlFor="email">Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <button type="submit">Register</button>
       </form>
       {error && <p>{error}</p>}
-      <Link to="/login">Already have an account? Login here.</Link>
+      <Link to="/">Already have an account? Login here.</Link>
     </div>
   );
 };
