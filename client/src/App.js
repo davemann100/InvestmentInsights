43594226ebc47e62fe311d530970f0028fe7610f
RenatoAZegarra <Zegarrarenatoa@gmail.com>
2023-05-24 17:27:39 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import Registration from './components/Registration';
 import Dashboard from './components/Dashboard';
 import Create from './components/Create';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
+  const shouldShowSidebar = !['/', '/register'].includes(location.pathname);
+
   return (
     <div className="App">
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+        {shouldShowSidebar && (
+          <Sidebar>
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/create" element={<Create />} />
+            </Routes>
+          </Sidebar>
+        )}
     </div>
   );
 }
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import { BiAddToQueue,
+BiAlignMiddle, 
+BiData,
+BiEdit,
+BiDesktop,
+BiLogOut
+} from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const Sidebar = ({children}) => {
+    const[isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const menuItem = [
+        {
+            path:"/edit/:id",
+            name: "Edit Profile",
+            icon: <BiEdit/>,
+        },
+        {
+            path:"/dashboard",
+            name: "Dashboard",
+            icon: <BiDesktop/>,
+        },
+        {
+            path:"/create",
+            name: "Create",
+            icon: <BiAddToQueue/>,
+        },
+        {
+            path:"/metrics",
+            name: "Metrics",
+            icon: <BiData/>,
+        },
+        {
+            path:"/logout",
+            name: "Log Out",
+            icon: <BiLogOut/>,
+        },
+
+    ]
+    return(
+        <div className="container">
+            <div style={{width: isOpen ? "300px" : "50px" }} className="sidebar">
+                <div className="top_section">
+                    <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                    <div  style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                        <BiAlignMiddle onClick={toggle}/>
+                    </div>
+                </div>
+                {
+                    menuItem.map((item, index) =>(
+                        <Link to={item.path} key={index} className="link" activeclassName="active">
+                            <div className="icon">{item.icon}</div>
+                            <div  style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                        </Link>
+                    ))
+                }
+            </div>
+            <main>{children}</main>
+        </div>
+    )
+}
+
+export default Sidebar;
